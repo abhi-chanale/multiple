c5abc28b6ab27881b298d54bcc38b4fee5f9f66c
abhi-chanale <109659976+abhi-chanale@users.noreply.github.com>
2022-08-04 16:01:56 +0530

    Add files via upload
    commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System;  
+   public class Animal
+{
+    public void eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+public class Dog : Animal
+{
+    public void run()
+    {
+        Console.WriteLine("Running...");
+    }
+}
+public class Cat : Animal, Dog
+{
+    public void sleep()
+    {
+        Console.WriteLine("Sleeping...");
+    }
+}
+class TestInheritance2
+{
+    public static void Main(string[] args)
+    {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.run();
+
+        Cat c1 = new Cat();
+        c1.eat();
+        c1.sleep();
+        Console.ReadLine();
+    }
+}
